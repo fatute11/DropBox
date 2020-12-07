3e8cd2fb2ccaab4fa8ae58fc6c3cda72c6291907
mehdi <mehdimedjdoub@gmail.com>
2020-12-08 00:36:04 +0100

    use main.js unstead of index.js to launch the server

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./src/routes')
 const cors = require("cors");
+require('dotenv').config()
 
 const app = express()
 
@@ -15,64 +16,66 @@ const www = process.env.WWW || './public';
 // Middelware
 app.use(express.static(www));
 app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes)
 
 const db = require("./src/models");
 const Role = db.role;
 // routes
-// require('./routes/auth.routes')(app);
-// require('./routes/user.routes')(app);
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
 
-// db.mongoose
-//   .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => {
-//     console.log("Successfully connect to MongoDB.");
-//     initial();
-//   })
-//   .catch(err => {
-//     console.error("Connection error", err);
-//     process.exit();
-//   });
+db.mongoose
+  .connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
 
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
 
-// function initial() {
-//   Role.estimatedDocumentCount((err, count) => {
-//     if (!err && count === 0) {
-//       new Role({
-//         name: "user"
-//       }).save(err => {
-//         if (err) {
-//           console.log("error", err);
-//         }
+        console.log("added 'user' to roles collection");
+      });
 
-//         console.log("added 'user' to roles collection");
-//       });
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
 
-//       new Role({
-//         name: "moderator"
-//       }).save(err => {
-//         if (err) {
-//           console.log("error", err);
-//         }
+        console.log("added 'moderator' to roles collection");
+      });
 
-//         console.log("added 'moderator' to roles collection");
-//       });
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
 
-//       new Role({
-//         name: "admin"
-//       }).save(err => {
-//         if (err) {
-//           console.log("error", err);
-//         }
-
-//         console.log("added 'admin' to roles collection");
-//       });
-//     }
-//   });
-// }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
