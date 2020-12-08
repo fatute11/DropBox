4666a816d782b79fc5bf18c830b8b80fe95acea5
mehdi <mehdimedjdoub@gmail.com>
2020-12-08 22:39:22 +0100

    [refactoring][models] add db connexion in models folder and update userModel

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./src/routes')
 const cors = require("cors");
-require('dotenv').config()
+
+const helmet = require('helmet');
+const morgan = require('morgan');
+const {startDatabase} = require('./src/database/mongo');
+const {insertAd, getAds} = require('./src/database/ads');
 
 const app = express()
 
@@ -13,69 +17,36 @@ var corsOptions = {
 const port = process.env.PORT || 8020; 
 const www = process.env.WWW || './public';
 
+// defining an array to work as the database (temporary solution)
+const ads = [
+  {title: 'Hello, world (again)!'}
+];
+
 // Middelware
 app.use(express.static(www));
 app.use(cors(corsOptions));
+// adding Helmet to enhance your API's security
+app.use(helmet());
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes)
 
-const db = require("./src/models");
-const Role = db.role;
 // routes
 require('./src/routes/auth.routes')(app);
 require('./src/routes/user.routes')(app);
 
-db.mongoose
-  .connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-    initial();
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
+// // replace the endpoint responsible for the GET requests
+// app.get('/', async (req, res) => {
+//   res.send(await getAds());
+// });
 
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "user"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'user' to roles collection");
-      });
-
-      new Role({
-        name: "moderator"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'moderator' to roles collection");
-      });
-
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-    }
-  });
-}
+// // start the in-memory MongoDB instance
+// startDatabase().then(async () => {
+//   await insertAd({title: 'Hello, now from the in-memory database!'});
+// });
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
